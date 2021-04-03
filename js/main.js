07d018aba34922c2666cd2292d97652d88709dd5
Pedro Lázaro <78751025+lazarp@users.noreply.github.com>
2021-04-03 20:51:26 -0300

    carrosel: versão jQuery removida

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-})
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav:false,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:3
+//         },
+//         1000:{
+//             items:5
+//         }
+//     }
+// })
 
 document.getElementById("watch-from-poster").onclick = function () {
         location.href = "video-play.html";
